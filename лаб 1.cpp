fe92e941feccfa675f0c7073a763fe6511cb1139
MAFIX52 <149573776+MAFIX52@users.noreply.github.com>
2023-11-07 10:21:38 +0300

    Create лаб 1.cpp

---
+++ лаб 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    short int a, b, i;
+    cout << "ведите первое число: ";
+    cin >> a;
+    cout << "ведите второе число: ";
+    cin >> b;
+    cout << "ведите рузультат умножения: ";
+    cin >> i;
+    if (i == a * b) {
+        cout << "все правильно!";
+    }
+    else cout << "НЕПРАВИЛЬНО! правильно будет так: >> " << a * b;
+    return 0;
+
+}
